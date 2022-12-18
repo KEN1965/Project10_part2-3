3935b8213713c788b55680dffbefc6c31bbeccc3
Kenichi Takahama <hammer528@msn.com>
2022-12-18 15:00:08 +0900

    Initial Commit

---
+++ Project10_part2/Project10_part2App.swift
@@ -0,0 +1,17 @@
+//
+//  Project10_part2App.swift
+//  Project10_part2
+//
+//  Created by K.Takahama on R 4/12/18.
+//
+
+import SwiftUI
+
+@main
+struct Project10_part2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
